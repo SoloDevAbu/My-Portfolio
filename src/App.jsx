41f620412cff489e6b810adb419db0cd6061ccd3
SoloDevAbu <abubakkar2502@gmail.com>
2025-01-01 19:29:18 +0530

    Added one More Projects and Resolve the scrolling issue with the background

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,11 @@ export default function App() {
   return (
     <div className="flex">
       <BrowserRouter>
-        <div className="hidden sm:flex w-1/4 h-screen justify-center bg-dots-pattern" style={{ backgroundSize: '8px 8px' }}>
+        <div className="hidden sm:flex w-1/4 top-0 left-0 h-screen justify-center bg-dots-pattern" style={{ backgroundSize: '8px 8px' }}>
           <Sidebar/>
         </div>
         
-        <div className="w-full h-screen sm:w-3/4 bg-dots-pattern" style={{ backgroundSize: '20px 20px' }} >
+        <div className="w-full h-screen sm:w-3/4 overflow-auto bg-dots-pattern" style={{ backgroundSize: '20px 20px' }} >
         <div className="flex w-full justify-end">
           <div className="flex fixed px-6 py-4 z-50 sm:hidden">
             <Topbar/>
